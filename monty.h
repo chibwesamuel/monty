9c2b12c338e88826457301e8a9dfb1e4fba65413
honokini <104803215+honokini@users.noreply.github.com>
2022-12-15 19:58:43 +0000

    monty.h

--- monty.h
+++ monty.h
@@ -16,7 +16,7 @@
 extern char **op_toks;
 
 /**
- * ct stack_s - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
