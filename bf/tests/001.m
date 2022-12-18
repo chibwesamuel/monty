f00707a876ca4aa861cfff5fa30db9ee93f790dc
honokini <104803215+honokini@users.noreply.github.com>
2022-12-18 19:58:56 +0000

    Commit all files in this projects

---
+++ bf/tests/001.m
@@ -0,0 +1,11 @@
+push 0 push 0 unto the stack$
+push 1 push 1 unto the stack$
+push 2
+push 3
+pall
+push hola
+
+push 5
+push 6
+
+pall this is the end of our program. Monty is awesome!
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+* f_nop - nothing
+* @head: stack head
+* @counter: no used
+* Return: no return
+*/
+void f_nop(stack_t **head, unsigned int counter)
+{
+    (void)counter;
+    (void)head;
+}
