b9bc93dda1de269738e64e2dfb3bbb7af8434f08
Samuel Mukosa Chibwe <102081659+chibwesamuel@users.noreply.github.com>
2022-12-18 08:26:02 +0200

    Fixed Error in errors_1.c

--- errors_1.c
+++ errors_1.c
@@ -9,35 +9,35 @@ int no_int_error(unsigned int line_number);
 /**
  * usage_error - Prints usage error messages.
  *
- * Return: (EXIT_FAILURE) always.
+ * Return: -1 always
  */
 int usage_error(void)
 {
 	fprintf(stderr, "USAGE: monty file\n");
-	return (EXIT_FAILURE);
+	return (-1);
 }
 
 /**
  * malloc_error - Prints malloc error messages.
  *
- * Return: (EXIT_FAILURE) always.
+ * Return: -1 always
  */
 int malloc_error(void)
 {
 	fprintf(stderr, "Error: malloc failed\n");
-	return (EXIT_FAILURE);
+	return (-1);
 }
 
 /**
  * f_open_error - Prints file opening error messages w/ file name.
  * @filename: Name of file failed to open
  *
- * Return: (EXIT_FAILURE) always.
+ * Return: -1 always
  */
 int f_open_error(char *filename)
 {
 	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	return (EXIT_FAILURE);
+	return (-1);
 }
 
 /**
@@ -45,23 +45,23 @@ int f_open_error(char *filename)
  * @opcode: Opcode where error occurred.
  * @line_number: Line number in Monty bytecodes file where error occured.
  *
- * Return: (EXIT_FAILURE) always.
+ * Return: -1 always
  */
 int unknown_op_error(char *opcode, unsigned int line_number)
 {
 	fprintf(stderr, "L%u: unknown instruction %s\n",
 		line_number, opcode);
-	return (EXIT_FAILURE);
+	return (-1);
 }
 
 /**
  * no_int_error - Prints invalid monty_push argument error messages.
  * @line_number: Line number in Monty bytecodes file where error occurred.
  *
- * Return: (EXIT_FAILURE) always.
+ * Return: -1 always
  */
 int no_int_error(unsigned int line_number)
 {
 	fprintf(stderr, "L%u: usage: push integer\n", line_number);
-	return (EXIT_FAILURE);
+	return (-1);
 }
