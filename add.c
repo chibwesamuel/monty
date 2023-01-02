1b8cadbcdd68c76135f4bcdffd1b875f2bfdbf7f
chibwesamuel <smchibwe@gmail.com>
2023-01-02 03:36:14 -0800

    removed white lines in main.c

--- add.c
+++ add.c
@@ -33,3 +33,20 @@ void f_add(stack_t **head, unsigned int counter)
 	 *head = h->next;
 	 free(h);
 }
+if ($line =~ /^\+.*\015/) {
+			my $herevet = "$here\n" . cat_vet($rawline) . "\n";
+			if (ERROR("DOS_LINE_ENDINGS",
+				  "DOS line endings\n" . $herevet) &&
+			    $fix) {
+				$fixed[$fixlinenr] =~ s/[\s\015]+$//;
+			}
+		} elsif ($rawline =~ /^\+.*\S\s+$/ || $rawline =~ /^\+\s+$/) {
+			my $herevet = "$here\n" . cat_vet($rawline) . "\n";
+			if (ERROR("TRAILING_WHITESPACE",
+				  "trailing whitespace\n" . $herevet) &&
+			    $fix) {
+				$fixed[$fixlinenr] =~ s/\s+$//;
+			}
+
+			$rpt_cleaners = 1;
+}
